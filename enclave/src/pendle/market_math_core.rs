60c30b8363dac9aff0e1d7cf6773d4ed3729f2e7
Aleksandr Cherenkov <aleksandr.cherenkov.3@gmail.com>
2025-08-15 20:00:25 +0000

    implement YT.newIndex() request

--- enclave/src/pendle/market_math_core.rs
+++ enclave/src/pendle/market_math_core.rs
@@ -1,39 +1,15 @@
 // implements structs and functions from MarketMathCore.sol
 // https://github.com/pendle-finance/pendle-core-v2-public/blob/6573ff85ca28b0f4fb5f6b7e2a1468fa7d0aa00b/contracts/core/Market/MarketMathCore.sol
 
-use crate::utils;
-use crate::eth;
-use crate::timing;
-// use crate::eth::signed::I256;
-// use crate::eth::signed::int::I256;
-
-
-use std::{collections::HashMap, ffi::CString, os::raw::c_char, str::FromStr};
 
 use crate::{
-    attestation_data::{AttestationPayload, ProvingResultOutput, SlotProofData},
     eth::{
-        block::Block,
-        header::Header,
-        signed::int::I256,
+        aliases::I256,
         math::{ln, exp},
-        primitives::{Address, U256},
-        proof::ProofResponse,
+        primitives::{U256},
     },
     timing::{Lap, Timings},
-    trie::verify_proof,
-    utils::{
-        construct_report_data, get_semantic_u256_bytes, keccak256, parse_slots_to_prove,
-        extract_storage_slots_with_merkle_proving,
-        RpcResponse,
-        StorageProvingConfig,
-    },
 };
-
-const ONE_17: I256 = I256::from_limbs([100_000_000_000_000_000u64, 0, 0, 0]);
-const ONE_20: I256 = I256::from_limbs([0x6bc75e2d63100000, 0x5, 0, 0]);
-const ONE_36: I256 = I256::from_limbs([0x2b878fe800000000, 0x13426172c74d82, 0, 0]);
-
 
 
 pub const IMPLIED_RATE_TIME: U256 = U256::from_limbs([86400*365, 0, 0, 0]);
@@ -48,13 +24,10 @@ pub(crate) struct MarketState {
     pub(crate) total_sy: I256, //int256 total_sy;
     // int256 totalLp; // is used only in ad add liquidity and remove liquidity
     // address treasury; // remove because we don't anything anywhere, just calc price
-    /// immutable variables ///
     pub(crate) scalar_root: I256, //int256 scalar_root;
     pub(crate) expiry: U256, //uint256 expiry;
-    /// fee data ///
     pub(crate) ln_fee_rate_root: U256, //uint256 ln_fee_rate_root;
-    pub(crate) reserve_fee_percent: U256, //uint256 reserveFeePercent; // base 100
-    /// last trade data ///
+    // uint256 reserveFeePercent; is never used
     pub(crate) last_ln_implied_rate: U256, // uint256 last_ln_implied_rate; // maybe should remove too
 }
 
@@ -74,16 +47,10 @@ pub(super) fn swap_exact_pt_for_sy(
     block_time: U256
 ) -> I256 { //net_sy_to_account
 
-    println! ("execute_trade_core start");
-
-    println! ("let neg1");
-    // TODO: enclave crases on I256::unchecked_from(-1) for some reason
     let neg1 = I256::MINUS_ONE;
 
-    println! ("let i_exact_pt_to_market");
     let i_exact_pt_to_market = I256::from_raw(exact_pt_to_market.0);
 
-    println! ("let neg_pt_to_market");
     let neg_pt_to_market = neg1 * i_exact_pt_to_market;
 
     let net_sy_to_account = execute_trade_core(
@@ -93,8 +60,6 @@ pub(super) fn swap_exact_pt_for_sy(
         neg_pt_to_market, // function is the same for both directions, this var is neg for pt->sy
         block_time
     );
-
-    println! ("execute_trade_core end");
 
     return net_sy_to_account;
 
@@ -107,39 +72,19 @@ fn execute_trade_core(
         // TODO think if we should update this logic to be get rid of ints and use uint instead
         net_pt_to_account: I256, // * -1, // function is the same for both directions, this var is neg for pt->sy
         block_time: U256
-//         MarketState memory market,
-//         PYIndex index,
-//         int256 net_pt_to_account,
-//         uint256 blockTime
-) -> I256 { //net_sy_to_account
-    // todo do we need this check here?
-//         if (MiniHelpers.isExpired(market.expiry, blockTime)) revert Errors.MarketExpired();
-    // todo do we need this check here?
-//         if (market.total_pt <= net_pt_to_account) revert Errors.MarketInsufficientPtForTrade(market.total_pt, net_pt_to_account);
+) -> I256 {
+    assert! (I256::from_raw(market.expiry.0) > I256::from_raw(block_time.0), "revert Errors.MarketExpired()");
+    assert! (market.total_pt > net_pt_to_account, "revert Errors.MarketInsufficientPtForTrade({}, {})", market.total_pt, net_pt_to_account);
 
-//         /// ------------------------------------------------------------
-//         /// MATH
-//         /// ------------------------------------------------------------
-    println! ("let comp");
     let comp: MarketPreCompute = get_market_pre_compute(
         market,
         index,
         block_time
     );
 
-    println! ("let net_sy_to_account");
-
     let net_sy_to_account = calc_trade(market, comp, index, net_pt_to_account);
 
     net_sy_to_account
-
-//         /// ------------------------------------------------------------
-//         /// WRITE
-//         /// ------------------------------------------------------------
-//         _setNewMarketStateTrade(market, comp, index, net_pt_to_account, net_sy_to_account, netSyToReserve, blockTime); // doesn't affect trade price but has revert, so maybe need to implement too
-//     }
-//     ////MATH
-
 }
 
 fn get_market_pre_compute(
@@ -148,13 +93,14 @@ fn get_market_pre_compute(
     block_time: U256,
 ) -> MarketPreCompute {
 
-
-    // assert! (market.expiry <= block_time); // TODO implement <= and uncomment
+    assert! (I256::from_raw(market.expiry.0) > I256::from_raw(block_time.0), "revert Errors.MarketExpired()");
     let time_to_expiry: U256 = market.expiry - block_time;
     let rate_scalar = get_rate_scalar(market, time_to_expiry);
     let total_asset = I256::from_raw(sy_to_asset(index, U256::from_i256(market.total_sy).unwrap()).0);
 
+    tracing::debug!("market.total_pt: {}", market.total_pt);
     assert! (market.total_pt != I256::ZERO, "revert Errors.MarketZeroTotalPtOrTotalAsset");
+    tracing::debug!("total_asset: {}", total_asset);
     assert! (total_asset != I256::ZERO, "revert Errors.MarketZeroTotalPtOrTotalAsset");
 
     let rate_anchor = get_rate_anchor(
@@ -308,7 +254,10 @@ fn get_exchange_rate_from_implied_rate(
     ln_implied_rate: U256,
     time_to_expiry: U256
 ) -> I256 /*exchange_rate*/ {
+    tracing::debug! ("ln_implied_rate: {}", ln_implied_rate);
+    tracing::debug! ("time_to_expiry: {}", time_to_expiry);
     let rt: U256 = (ln_implied_rate * time_to_expiry) / IMPLIED_RATE_TIME;
+    tracing::debug! ("rt: {}", rt);
 
     let rt_signed = I256::from_raw(rt.0);
 
--- enclave/src/pendle/mod.rs
+++ enclave/src/pendle/mod.rs
@@ -1,26 +1,17 @@
 mod market_math_core;
 
-use anyhow::Error;
-
 use crate::utils;
 use crate::eth;
-use crate::timing;
 
-use std::{collections::HashMap, ffi::CString, os::raw::c_char, str::FromStr};
+use std::collections::HashMap;
 
 use crate::{
-    attestation_data::{AttestationPayload, ProvingResultOutput, SlotProofData},
     eth::{
-        block::Block,
-        header::Header,
-        signed::int::I256,
+        aliases::I256,
         primitives::{Address, B256, U256},
-        proof::ProofResponse,
     },
-    timing::{Lap, Timings},
-    trie::verify_proof,
+    timing::Timings,
     utils::{
-        construct_report_data, get_semantic_u256_bytes, keccak256, parse_slots_to_prove,
         extract_storage_slots_with_merkle_proving,
         make_http_request,
         RpcResponse,
@@ -33,24 +24,24 @@ struct ImmutablesFromMarketState {
     scalar_root_from_storage: I256,
     expiry_from_storage: U256,
     ln_fee_rate_root_from_storage: U256,
-    reserve_fee_percent_from_storage: U256,
 }
 
 fn get_immutables_from_market_state(
     storage_proving_config: utils::StorageProvingConfig,
 ) -> anyhow::Result<ImmutablesFromMarketState> {
-    let selector = "794052f3"; // readState(address)
+    let selector = "794052f3"; // pendle-core-v2-public$ forge selectors list | grep readState(address)
     // pendle router v4 form here https://docs.pendle.finance/Developers/Contracts/PendleRouter
     let router_param = "000000000000000000000000888888888889758F76e7103c6CbF23ABbF58F946";
     let call_data = format!("{}{}", selector, router_param);
 
+    println! ("&storage_proving_config.block_number: {}", &storage_proving_config.block_number);
     let eth_call_payload = serde_json::json!({
         "jsonrpc": "2.0",
         "method": "eth_call",
         "params": [{
             "to": &storage_proving_config.address,
             "data": call_data
-        }, "latest"],
+        }, &storage_proving_config.block_number],
         "id": 1
     })
     .to_string();
@@ -63,7 +54,7 @@ fn get_immutables_from_market_state(
 
     let rpc_proof_response: RpcResponse<String> = serde_json::from_str(&eth_call_response_str)
         .map_err(|e| anyhow::anyhow!("Failed to parse proof RPC response: {}", e))?;
-    tracing::info!("Proof received successfully");
+    tracing::info!("Request result parsing succeeded");
    let hex_data = rpc_proof_response.result;
 
     // struct MarketState {
@@ -107,12 +98,82 @@ fn get_immutables_from_market_state(
         scalar_root_from_storage: scalar_root,
         expiry_from_storage: expiry,
         ln_fee_rate_root_from_storage: last_ln_implied_rate,
-        reserve_fee_percent_from_storage: reserve_fee_percent,
     };
 
     Ok(res)
 
 }
+
+fn get_yt_index(
+    storage_proving_config: utils::StorageProvingConfig,
+) -> anyhow::Result<U256> {
+    let read_tokens_selector = "2c8ce6bc"; // pendle-core-v2-public$ forge selectors list | grep readTokens()
+
+    let read_tokens_eth_call_payload = serde_json::json!({
+        "jsonrpc": "2.0",
+        "method": "eth_call",
+        "params": [{
+            "to": &storage_proving_config.address,
+            "data": read_tokens_selector
+        }, &storage_proving_config.block_number],
+        "id": 1
+    })
+    .to_string();
+
+    let read_tokens_eth_call_response_str = make_http_request(&storage_proving_config.rpc_url, "POST", read_tokens_eth_call_payload.as_bytes())
+        .map_err(|e| anyhow::anyhow!("eth_call HTTP request failed: {:?}", e))?;
+
+    println! ("read_tokens_eth_call_response_str: {}", read_tokens_eth_call_response_str);
+
+
+    let read_tokens_rpc_proof_response: RpcResponse<String> = serde_json::from_str(&read_tokens_eth_call_response_str)
+        .map_err(|e| anyhow::anyhow!("Failed to parse proof RPC response: {}", e))?;
+    tracing::info!("Request result parsing succeeded");
+    let read_tokens_hex_data = read_tokens_rpc_proof_response.result;
+
+    // returns (IStandardizedYield _SY, IPPrincipalToken _PT, IPYieldToken _YT)
+    let read_tokens_data = hex::decode(&read_tokens_hex_data[2..])?;
+
+    let yt_address_bytes: [u8; 20] = read_tokens_data[64+12..64+32].try_into()?;
+    let yt_address = Address::from(yt_address_bytes);
+
+    println! ("yt_address: {}", yt_address);
+//////////////////////
+    let py_index_current_selector = "1d52edc4"; // forge selectors list | grep 'pyIndexCurrent()'
+
+    let py_index_current_eth_call_payload = serde_json::json!({
+        "jsonrpc": "2.0",
+        "method": "eth_call",
+        "params": [{
+            "to": "48bbbedc4d2491cc08915d7a5c7cc8a8edf165da", // TODO unhardcode
+            "data": py_index_current_selector
+        }, &storage_proving_config.block_number],
+        "id": 1
+    })
+    .to_string();
+
+    let py_index_current_eth_call_response_str = make_http_request(&storage_proving_config.rpc_url, "POST", py_index_current_eth_call_payload.as_bytes())
+        .map_err(|e| anyhow::anyhow!("eth_call HTTP request failed: {:?}", e))?;
+
+    println! ("py_index_current_eth_call_response_str: {}", py_index_current_eth_call_response_str);
+
+
+    let py_index_current_rpc_proof_response: RpcResponse<String> = serde_json::from_str(&py_index_current_eth_call_response_str)
+        .map_err(|e| anyhow::anyhow!("Failed to parse proof RPC response: {}", e))?;
+    tracing::info!("Request result parsing succeeded");
+    let py_index_current_hex_data = py_index_current_rpc_proof_response.result;
+
+    // returns (IStandardizedYield _SY, IPPrincipalToken _PT, IPYieldToken _YT)
+    let py_index_current_data = hex::decode(&py_index_current_hex_data[2..])?;
+
+    let py_index_current = U256::from_be_bytes::<32>(py_index_current_data[0..32].try_into()?);
+
+    println! ("yt_index: {}", py_index_current);
+
+    Ok(py_index_current) // TODO actually need to return also YT address
+
+}
+
 
 
 pub fn pendle_logic(
@@ -121,15 +182,10 @@ pub fn pendle_logic(
     total_timer_start: std::time::Instant,
 ) -> anyhow::Result<I256> { // TODO fugure out what to return from here
 
-    // looks like there will be requests form three storages: market, router, pendleERC20:
-    // https://github.com/pendle-finance/pendle-core-v2-public/blob/6ca87b6e5a823d603cb8f66f983f5bc63b53218a/contracts/core/Market/v3/PendleMarketV3.sol#L278-L285
-    // let's start with info only from market contract, it's the majority of the requested variables
-
-
-
     let market_storage_proving_config = StorageProvingConfig {
         rpc_url: storage_proving_config.rpc_url.clone(),
         address: storage_proving_config.address.clone(),
+        // TODO maybe more beautyful way to declare than array
         storage_slots: vec![
             eth::primitives::FixedBytes([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,10]),
             eth::primitives::FixedBytes([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,11]),
@@ -140,12 +196,7 @@ pub fn pendle_logic(
 
     let market_storage_storage_slots = extract_storage_slots_with_merkle_proving(market_storage_proving_config.clone(), timings, total_timer_start)?;
 
-    println! ("let test");
-
-    let test: U256 = U256::from_b256(market_storage_storage_slots.proven_slots[0].value_unhashed);
-
     let block_timestamp = U256::from_u64(market_storage_storage_slots.block_timestamp);
-
 
     let mut market_storage_hashmap: HashMap<i32, B256> = HashMap::new();
     market_storage_hashmap.insert(10, market_storage_storage_slots.proven_slots[0].value_unhashed);
@@ -162,91 +213,92 @@ pub fn pendle_logic(
     //     // 1 SLOT = 144 bits
     // }
 
-    // println!("Pendle contract storage slot 10: {}", market_storage_hashmap.get(&10));
-
     let raw_slot_10: [u8; 32] = market_storage_hashmap.get(&10)
         .ok_or_else(|| anyhow::anyhow!("error: slot 10 not found"))?
             .0;
+    tracing::debug! ("raw_slot_10: {}", hex::encode(raw_slot_10));
 
     // int128 totalPt is first 16 bytes
     let mut pt_bytes = [0u8; 16];
     pt_bytes.copy_from_slice(&raw_slot_10[16..32]);
     let total_pt_from_storage: i128 = i128::from_be_bytes(pt_bytes);
-    println!("total_pt_from_storage = {}", total_pt_from_storage);
+    tracing::debug!("total_pt_from_storage = {}", total_pt_from_storage);
 
     // int128 totalSy is next 16 bytes
     let mut sy_bytes = [0u8; 16];
     sy_bytes.copy_from_slice(&raw_slot_10[0..16]);
     let total_sy_from_storage: i128 = i128::from_be_bytes(sy_bytes);
-    println!("total_sy_from_storage = {}", total_sy_from_storage);
+    tracing::debug!("total_sy_from_storage = {}", total_sy_from_storage);
 
 
     let raw_slot_11: [u8; 32] = market_storage_hashmap.get(&11)
         .ok_or_else(|| anyhow::anyhow!("error: slot 11 not found"))?
             .0;
-
-    println! ("raw_slot_10: {}", hex::encode(raw_slot_10));
-    println! ("raw_slot_11: {}", hex::encode(raw_slot_11));
+    tracing::debug! ("raw_slot_11: {}", hex::encode(raw_slot_11));
 
     // uint96 lastLnImpliedRate is first 12 bytes
     let mut ln_rate_bytes = [0u8; 16];
     ln_rate_bytes[4..16].copy_from_slice(&raw_slot_11[32-12..32]);
     let ln_rate_from_storage: u128 = u128::from_be_bytes(ln_rate_bytes);
-    println!("ln_rate_from_storage = {}", ln_rate_from_storage);
+    tracing::debug!("ln_rate_from_storage = {}", ln_rate_from_storage);
 
     // uint16 observationIndex is next 2 bytes
     let mut obs_index_bytes = [0u8; 16];
     obs_index_bytes[14..16].copy_from_slice(&raw_slot_11[32-12-2..32-12]);
     let obs_index_from_storage: i128 = i128::from_be_bytes(obs_index_bytes);
-    println!("obs_index_from_storage = {}", obs_index_from_storage);
+    tracing::debug!("obs_index_from_storage = {}", obs_index_from_storage);
 
     // uint16 observationCardinality is next 2 bytes
     let mut obs_card_bytes = [0u8; 16];
     obs_card_bytes[14..16].copy_from_slice(&raw_slot_11[32-12-2-2..32-12-2]);
     let obs_card_from_storage: i128 = i128::from_be_bytes(obs_card_bytes);
-    println!("obs_card_from_storage = {}", obs_card_from_storage);
+    tracing::debug!("obs_card_from_storage = {}", obs_card_from_storage);
 
     // uint16 observationCardinalityNext is next 2 bytes
     let mut obs_card_next_bytes = [0u8; 16];
     obs_card_next_bytes[14..16].copy_from_slice(&raw_slot_11[32-12-2-2-2..32-12-2-2]);
     let obs_card_next_from_storage: i128 = i128::from_be_bytes(obs_card_next_bytes);
-    println!("obs_card_next_from_storage = {}", obs_card_next_from_storage);
+    tracing::debug!("obs_card_next_from_storage = {}", obs_card_next_from_storage);
 
-    let immutables = get_immutables_from_market_state(market_storage_proving_config)
+    let immutables = get_immutables_from_market_state(market_storage_proving_config.clone())
         .map_err(|e| anyhow::anyhow!("Immutables request failed: {:?}", e))?;
 
     let scalar_root_from_storage = immutables.scalar_root_from_storage;
     let expiry_from_storage = immutables.expiry_from_storage;
     let ln_fee_rate_root_from_storage = immutables.ln_fee_rate_root_from_storage;
-    let reserve_fee_percent_from_storage = immutables.reserve_fee_percent_from_storage;
 
 
     // in solidity market is constructed in PendleParketV3.readState,
     // but here we access storage in mod.rs, so I decided to construct market here
     // and pass it to swap_exact_pt_for_sy as imput
-    println! ("let market");
     let market: market_math_core::MarketState = market_math_core::MarketState {
         total_pt: I256::try_from(total_pt_from_storage).unwrap(),
         total_sy: I256::try_from(total_sy_from_storage).unwrap(),
         scalar_root: scalar_root_from_storage,
         expiry: expiry_from_storage,
         ln_fee_rate_root: ln_fee_rate_root_from_storage,
-        reserve_fee_percent: reserve_fee_percent_from_storage,
         last_ln_implied_rate: U256::new(ln_rate_from_storage),
     };
 
-    println! ("let index");
-    let index = U256::ONE; // TODO YT.newIndex()
-    let exact_pt_in: U256 = U256::from_limbs([1000000, 0, 0, 0]); // TODO pass as cmd line arg or smth like that
+    let yt_index = get_yt_index(market_storage_proving_config)
+        .map_err(|e| anyhow::anyhow!("YT.newIndex() request failed: {:?}", e))?;
 
-    println! ("let exact_sy_out");
+
+    let billion: U256 = U256::from_limbs([1_000_000_000, 0, 0, 0]); // TODO pass as cmd line arg or smth like that
+    let exact_pt_in = billion * billion * U256::from_limbs([10_000_000, 0, 0, 0]);
+
     let exact_sy_out = market_math_core::swap_exact_pt_for_sy(
         market,
-        index,
+        yt_index,
         exact_pt_in,
         block_timestamp,
     );
-    println! ("let exact_sy_out ended");
+
+    tracing::info! ("storage_proving_config.block_number: {}", storage_proving_config.block_number);
+    tracing::info! ("exact_pt_in: {}", exact_pt_in);
+    tracing::info! ("exact_pt_in / 10**18: {}", exact_pt_in / U256::from_limbs([1000_000_000_000_000_000u64, 0, 0, 0]));
+    tracing::info! ("exact_sy_out: {}", exact_sy_out);
+    tracing::info! ("exact_sy_out / 10**18: {}", exact_sy_out/ I256::from_limbs([1000_000_000_000_000_000u64, 0, 0, 0]));
 
     Ok(exact_sy_out)
 }
